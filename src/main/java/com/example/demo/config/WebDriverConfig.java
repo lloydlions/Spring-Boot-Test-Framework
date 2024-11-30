0a3e0b43ce9bfb7b5df2dc8e90c93bd83dadb45e
Lloyd Lions <lloyd.lions@gmail.com>
2024-11-30 11:46:37 +0800

    screenshot capability

--- src/main/java/com/example/demo/config/WebDriverConfig.java
+++ src/main/java/com/example/demo/config/WebDriverConfig.java
@@ -3,11 +3,15 @@ package com.example.demo.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.Duration;
 
@@ -22,7 +26,24 @@ public class WebDriverConfig {
     private int timeout;
 
     @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver chromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+
+    @Bean
+//    @Primary
+    @ConditionalOnProperty(name = "browser", havingValue = "edge")
+    public WebDriver edgeDriver(){
+        WebDriverManager.edgedriver().setup();
+        return new EdgeDriver();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebDriver defaultDriver(){
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
     }
